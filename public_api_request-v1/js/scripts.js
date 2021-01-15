91621566514ecacf6b013270931f3232c9dfdad8
JC <jcbrady89@yahoo.com>
2021-01-15 13:53:53 -0700

    Working on Modal

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -9,14 +9,17 @@ function fetchData(url) {
   return fetch(url)
     .then(response => response.json())
     .then(data => generateHTML(data.results))
+    .then(data => generateMODAL(data.results))
     .catch(error => console.log("Looks like there was a problem", error))
 }
 
+// Generate HTML for 12 Cards
 function generateHTML(data) {
-  console.log(data.length)
-  console.log(data[0].name)
+  //console.log(data[0].name) //test data results
+
   const gallery = document.getElementById("gallery")
   const employeeCard = document.createElement("div")
+
   // loop through the data set, 12 results
   for (let i = 0; i < data.length; i++) {
     let html = `<div class="card">
@@ -30,11 +33,19 @@ function generateHTML(data) {
   </div>
 </div>`
 
-    // employeeCard.innerHTML += html
-    //gallery.appendChild(employeeCard)
     gallery.insertAdjacentHTML("beforeend", html)
-  }
+
+    // Get the HTML elements for the Modal window and listen for clicks
+    const card = document.querySelectorAll(".card")[i]
+    card.addEventListener("click", generateMODAL)
+  } // end loop
 }
+
+function generateMODAL(e, data) {
+  console.log(e.target)
+  console.log(data)
+}
+
 /**
  * element.insertAdjacentHTML('beforeend', 'HTML string')
  * That technique will allow you to add strings of HTML to the DOM without disrupting
