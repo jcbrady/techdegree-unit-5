d404bfdf4c1b577865e659c93f9c8e6f96e8be9b
JC <jcbrady89@yahoo.com>
2021-01-25 11:01:04 -0700

    Data in Modal

--- JS/scripts.js
+++ JS/scripts.js
@@ -16,8 +16,8 @@ function fetchData(url) {
   return fetch(url)
     .then(response => response.json()) // returns a promise and parses it to JSON
     .then(data => {
-      generateHTML(data.results); // calls function with the data results
-      // generateMODAL(data.results);
+      generateCards(data.results); // calls function with the data results
+      //generateModal(data.results); // generate modal
     })
     .catch(error => console.log("Looks like there was a problem", error));
 }
@@ -27,7 +27,7 @@ function fetchData(url) {
 
 // Generate HTML for 12 Cards
 // --------------------------------------
-function generateHTML(data) {
+function generateCards(data) {
   console.log(data);
   //console.log(data[0].name); //test data results
   const gallery = document.getElementById("gallery");
@@ -36,7 +36,7 @@ function generateHTML(data) {
   for (let i = 0; i < data.length; i++) {
     let html = `<div class="card">
   <div class="card-img-container">
-      <img class="card-img" src="${data[i].picture.thumbnail}" alt="profile picture">
+      <img class="card-img" src="${data[i].picture.medium}" alt="profile picture">
   </div>
   <div class="card-info-container">
       <h3 id="name" class="card-name cap">${data[i].name.first} ${data[i].name.last}</h3>
@@ -53,34 +53,40 @@ function generateHTML(data) {
     //// card.addEventListener("click", toggleMODAL);
   } // end loop
   const card = document.querySelectorAll(".card");
-  console.log(card);
+  //console.log(card);
   card.forEach(item => {
-    console.log(item);
-    // I'm not sure how to add a parameter (such as data) to a callback function in this event listener:
-    item.addEventListener("click", generateMODAL);
+    // pass (data) as parameter to generateModal with callback function
+    item.addEventListener("click", event => {
+      generateModal(data);
+    });
   });
-  //card.addEventListener("click", toggleMODAL);
-  // Call Function to generate MODAL html
-  //generateMODAL(data);
-}
 
-// Generate HTML for MODAL
+  // hide modal on page load
+  // const modalContainer = document.querySelector(".modal-container");
+  // console.log(modalContainer); // "null" but it works?
+  // modalContainer.style.display = "none";
+  //
+} // end generateCards() function
+
+//
 // --------------------------------------
-function generateMODAL(data) {
-  //console.log(data); // Array of 12 objects
-  // console.log(data[0]); // sometimes logs the first object, other times says undefined
-  //console.log(data[0].gender); // mysteriously does not work
-  //console.log(data.length);
-
-  //for (let i = 0; i < data.length; i++) {
-  let html = `
+// Generate HTML for Modal window
+// --------------------------------------
+function generateModal(data) {
+  console.log(data); // Array of 12 objects
+  console.log(data[0]);
+  console.log(data[0].gender);
+  console.log(data.length);
+  let html;
+  for (let i = 0; i < data.length; i++) {
+    html = `
   <div class="modal-container">
   <div class="modal">
       <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
       <div class="modal-info-container">
           <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-          <h3 id="name" class="modal-name cap">name</h3>
-          <p class="modal-text">email</p>
+          <h3 id="name" class="modal-name cap">${data[i].name.first} ${data[i].name.last}</h3>
+          <p class="modal-text">${data[i].email}</p>
           <p class="modal-text cap">city</p>
           <hr>
           <p class="modal-text">(555) 555-5555</p>
@@ -89,7 +95,7 @@ function generateMODAL(data) {
       </div>
   </div>
   `;
-  //}
+  }
 
   // Select and insert Modal Window
   const body = document.querySelector("body");
@@ -98,21 +104,34 @@ function generateMODAL(data) {
   // Close Modal Window
   const modalCloseBtn = document.getElementById("modal-close-btn");
   const modalContainer = document.querySelector(".modal-container");
-  const modalContent = document.querySelector(".modal-info-container");
-  const modalImg = document.querySelector(".modal-img"); // bug fix -
+
+  // possibly come back to this, (closing the modal window on background click)
+  // const modalContent = document.querySelector(".modal-info-container p");
+  // const modalImg = document.querySelector(".modal-img"); // bug fix -
+  // const modalName = document.querySelector(".modal-name"); // bug fix
+  // const modalText = document.querySelector(".modal-text"); // bug fix
+  // const modalP = modalContent.querySelectorAll(".modal-info-container p");
+  //console.log(modalP);
+  // Initially set modal to closed on page load?
+  // modalContainer.style.display = "none";
+  // Functionality to close Modal window if user clicks background
+  // bug fix - added all classes to conditional, so that the modal doesn't close when
+  // content area is clicked.
+  // modalContainer.addEventListener("click", e => {
+  //   if (e.target !== modalContent && e.target !== modalImg) {
+  //     modalContainer.style.display = "none";
+  //   }
+  // });
 
   modalCloseBtn.addEventListener("click", e => {
     modalContainer.style.display = "none";
-    console.log(e.target);
   });
+} // end generateModal() function
 
-  // Functionality to close Modal window if user clicks background
-  // bug fix - added modalImg to conditional, so Modal doesn't close when profile image is clicked
-  modalContainer.addEventListener("click", e => {
-    if (e.target !== modalContent && e.target !== modalImg) {
-      modalContainer.style.display = "none";
-    }
-  });
-
-  //return html;
-}
+// close modal window when page loads
+// window.addEventListener("load", event => {
+//   console.log("The page is loaded.");
+//   const modalContainer = document.querySelector(".modal-container");
+//   console.log(modalContainer);
+//   modalContainer.style.display = "none";
+// });
--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Unit 5 - Working with API's
 Additional Features to CSS
 • Added drop shadows to the 12 cards
 • Added a hover effect to subtly scale up the names and photos
+• Added background image to page for a textured effect
 
-Additional Features to JavaScript
-• Added functionality to close Modal window if user clicks background
+Consider Additional Features to JavaScript
+• I want to add functionality to close Modal window if user clicks the background, but this goal is sidetracking me right now. Will come back ot it
